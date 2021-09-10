700daaf1ae80d887dd70adc48e0b4b0ce983334d
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-09-10 14:40:32 +0200

    Initial commit
    Packages:
    - river
    - zig-doctest

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A collection of Zig packages";
+
+  inputs.zig-overlay.url = "github:arqv/zig-overlay";
+  inputs.zig-nightly.url = "github:chivay/zig-nightly";
+
+  outputs = { self, nixpkgs, zig-overlay, zig-nightly }: {
+
+    packages.x86_64-linux =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+      {
+
+        river = pkgs.callPackage ./pkgs/river { };
+
+        zig-doctest = pkgs.callPackage ./pkgs/zig-doctest {
+          zig = zig-nightly.defaultPackage."x86_64-linux";
+        };
+
+      };
+
+  };
+}
---
+++ pkgs/zig-doctest/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, zig
+}:
+
+stdenv.mkDerivation rec {
+  pname = "zig-doctest";
+  version = "unstable-2021-09-02";
+
+  src = fetchFromGitHub {
+    owner = "kristoff";
+    repo = pname;
+    rev = "e1e08298bf7b08b590ea8860ca2415d3e41b7d68";
+    sha256 = "D64XvYt3teS2Vi2ijIoDFYQjsDWRZZUM0Mir2+SyU5Y=";
+  };
+
+  nativeBuildInputs = [ zig ];
+
+  dontConfigure = true;
+
+  preBuild = ''
+    export HOME=$TMPDIR
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    zig build -Drelease-safe -Dcpu=baseline --prefix $out install
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/kristoff-it/zig-doctest";
+    description = "A tool for testing snippets of code";
+    license = licenses.mit;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ joachimschmidt557 ];
+  };
+}
