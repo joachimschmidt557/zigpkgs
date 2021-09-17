20172525d694395e34afdfe70751f49631094b17
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-09-17 11:34:57 +0200

    Use flake-utils.lib.eachSystem

--- flake.nix
+++ flake.nix
@@ -1,36 +1,46 @@
 {
   description = "A collection of Zig packages";
 
-  inputs.zig-overlay.url = "github:arqv/zig-overlay";
-  inputs.zig-nightly.url = "github:chivay/zig-nightly";
-
-  outputs = { self, nixpkgs, zig-overlay, zig-nightly }: {
-
-    packages.x86_64-linux =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-        buildZig081Project = pkgs.callPackage ./pkgs/development/build-zig-project.nix {
-          zig = pkgs.zig;
-        };
-        buildZigNightlyProject = pkgs.callPackage ./pkgs/development/build-zig-project.nix {
-          zig = zig-nightly.defaultPackage."x86_64-linux";
-        };
-      in
-      {
-
-        river = pkgs.callPackage ./pkgs/river {
-          buildZigProject = buildZig081Project;
-        };
-
-        vpkz = pkgs.callPackage ./pkgs/vpkz {
-          buildZigProject = buildZigNightlyProject;
-        };
-
-        zig-doctest = pkgs.callPackage ./pkgs/zig-doctest {
-          buildZigProject = buildZigNightlyProject;
-        };
-
-      };
-
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay.url = "github:arqv/zig-overlay";
+    zig-nightly.url = "github:chivay/zig-nightly";
   };
+
+  outputs =
+    { self, nixpkgs, flake-utils, zig-overlay, zig-nightly }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in
+    flake-utils.lib.eachSystem systems (system:
+    {
+
+      packages =
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          buildZig081Project = pkgs.callPackage ./pkgs/development/build-zig-project.nix {
+            zig = pkgs.zig;
+          };
+          buildZigNightlyProject = pkgs.callPackage ./pkgs/development/build-zig-project.nix {
+            zig = zig-nightly.defaultPackage.${system};
+          };
+        in
+        {
+
+          river = pkgs.callPackage ./pkgs/river {
+            buildZigProject = buildZig081Project;
+          };
+
+          vpkz = pkgs.callPackage ./pkgs/vpkz {
+            buildZigProject = buildZigNightlyProject;
+          };
+
+          zig-doctest = pkgs.callPackage ./pkgs/zig-doctest {
+            buildZigProject = buildZigNightlyProject;
+          };
+
+        };
+
+    }
+    );
 }
